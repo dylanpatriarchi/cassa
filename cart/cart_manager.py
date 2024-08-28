ae583583e9861a8d3a094f8ac1eaf670310c149f
dylan patriarchi <dylanpat2004@icloud.com>
2024-08-28 18:47:55 +0200

    added bash script for make virtual env, generation of pdf receipt

--- cart/cart_manager.py
+++ cart/cart_manager.py
@@ -1,3 +1,6 @@
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+
 class CartManager:
     def __init__(self):
         self.cart = []
@@ -14,3 +17,25 @@ class CartManager:
         total = sum(p['prezzo'] for p in self.cart)
         print(f"Carrello: {[p['nome'] for p in self.cart]}")
         print(f"Totale: €{total}")
+        user_input = input("Premi 'q' per stampare lo scontrino o qualsiasi altro tasto per continuare: ")
+        if user_input.lower() == 'q':
+            self.print_receipt()
+
+    def print_receipt(self):
+        filename = "scontrino.pdf"
+        c = canvas.Canvas(filename, pagesize=letter)
+        width, height = letter
+
+        c.drawString(100, height - 100, "Scontrino")
+        c.drawString(100, height - 120, "="*30)
+
+        y = height - 140
+        for product in self.cart:
+            c.drawString(100, y, f"Nome: {product['nome']} - Prezzo: €{product['prezzo']:.2f}")
+            y -= 20
+
+        total = sum(p['prezzo'] for p in self.cart)
+        c.drawString(100, y, f"Totale: €{total:.2f}")
+
+        c.save()
+        print(f"Scontrino salvato in {filename}")
